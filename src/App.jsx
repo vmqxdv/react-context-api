c4279ce82cb9f87f01aa40a81f40e32da0e984d7
vmqxdv <vquintilio@gmail.com>
2025-04-23 15:55:32 +0200

    feat: Impostate le rotte base

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { PostProvider } from './context/PostContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PostsPage from './pages/PostsPage';
+import PostDetails from './components/PostDetails';
+
 export default function App() {
   return (
-    <></>
-  )
-};
+    <PostProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<PostsPage />} />
+          <Route path='/:id' element={<PostDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </PostProvider>
+  );
+}
