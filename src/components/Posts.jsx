5084409c2f79f8d66efb1f4af08e3fbd862dbdde
vmqxdv <vquintilio@gmail.com>
2025-04-28 10:25:16 +0200

    Refactor: Rimossa la doppia chiamata API

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useContext } from 'react';
+import { PostContext } from '../context/PostContext';
 import { Link } from 'react-router-dom';
 
 export default function Posts() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        setPosts(response.data);
-      })
-  }, []);
+  const posts = useContext(PostContext);
 
   const displayPosts = (posts) => {
     return posts.map(post => (
       <div key={post.id}>
-        <h3><Link to={`${post.id}`}>{post.title}</Link></h3>
+        <h3><Link to={`/${post.id}`}>{post.title}</Link></h3>
       </div>
-    ))
+    ));
   };
 
   return (
